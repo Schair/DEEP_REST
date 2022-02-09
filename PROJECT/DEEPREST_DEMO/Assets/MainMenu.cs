ee20e5637b516f0c7b1ce5dc04c2a361706f91d8
Schair <49439492+Schair@users.noreply.github.com>
2022-02-09 21:36:26 +0100

    ANIMATIONS STARTED

--- PROJECT/DEEPREST_DEMO/Assets/MainMenu.cs
+++ PROJECT/DEEPREST_DEMO/Assets/MainMenu.cs
@@ -5,12 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame(){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    public Animator transitionAnim;
+    
+    public void PlayGame()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(LoadScene());
     }
 
-    public void QuitGame(){
+    public void QuitGame()
+    {
         Debug.Log("GAME CLOSED");
         Application.Quit();
+    }
+
+    IEnumerator LoadScene()
+    {
+        transitionAnim.SetTrigger("end");
+        yield return null;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        
     }
 }
