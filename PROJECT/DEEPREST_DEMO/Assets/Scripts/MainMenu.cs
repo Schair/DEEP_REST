ac844d59bb36496668de9e4e2feafc2cdde3460f
Schair <49439492+Schair@users.noreply.github.com>
2022-02-11 21:13:01 +0100

    ANIMATIONS AND DUMMY

---
+++ PROJECT/DEEPREST_DEMO/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{   
+    public Animator transition;
+
+    public float transitionTime = 1.0f;
+
+    public void PlayGame()
+    {
+        LoadNextLevel();
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("GAME CLOSED");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel(){
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex){
+        // Play animation
+        transition.SetTrigger("Start");
+        // Wait
+        yield return new WaitForSeconds(transitionTime);
+        // Load Scene
+        SceneManager.LoadScene(levelIndex);
+    }
+}
+
